053e92bd5af506efe8244406e6622af7c78e515e
habit456 <48520483+habit456@users.noreply.github.com>
2020-03-19 19:34:30 -0400

    Learning about beans

---
+++ src/com/company/Bean.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Bean implements java.io.Serializable {
+    private String name;
+    private int age;
+    private String brand;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
